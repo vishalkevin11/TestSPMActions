fd32fd10d4eadc74a905fab3ad4ae240068a7406
kevin vishal <vishalkevin11@gmail.com>
2024-09-13 19:31:55 +0530

    First

---
+++ TestSPMActions/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TestSPMActions
+//
+//  Created by kevin.saldanha on 13/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ TestSPMActions/TestSPMActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSPMActionsApp.swift
+//  TestSPMActions
+//
+//  Created by kevin.saldanha on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TestSPMActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
